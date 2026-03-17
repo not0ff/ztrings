6524c9c89657091aecd45846792ff29cf1b91ddd
not0ff <209936853+not0ff@users.noreply.github.com>
2026-03-17 18:12:09 +0100

    add the license notice

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,19 @@
+//     ztrings - retrieve printable strings from files
+//     Copyright (C) 2026-present  Not0ff
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 const std = @import("std");
 const builtin = @import("builtin");
 const build_options = @import("build_options");
